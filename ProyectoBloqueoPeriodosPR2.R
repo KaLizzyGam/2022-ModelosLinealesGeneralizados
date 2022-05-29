bd94d666f8e0249598d829ab570a5d1950a779b9
KaLizzyGam <lizzygamboap@gmail.com>
2022-05-28 19:50:12 -0500

    cambios AGAIN

--- ProyectoBloqueoPeriodosPR2.R
+++ ProyectoBloqueoPeriodosPR2.R
@@ -23,9 +23,6 @@ bloqueo <- bloqueo %>%
 
 skimr::skim(bloqueo)
 
-#Filtrado
-vect_names <- bloqueo %>% select(starts_with("VAS")) %>% select(-ends_with("DIS"),-VAS_AB) %>% names()
-vect_names
 
 funct_base <- function(bloqueo, vect_names, i){
 bloqueo_agrupada <- bloqueo %>% 
@@ -46,39 +43,10 @@ names(bloqueo_agrupada) <- nuevos_nombres
 bloqueo_agrupada
 }
 
- 
-
-#################### INICIANDO VARIABLES ####################
-
-dic_efc_ctes <- list()
-pR2_efc_ctes <- list()
-
-dic_efc_ind <- list()
-pR2_efc_ind <- list()
-
-dic_efc_int <- list()
-pR2_efc_int <- list()
-
-dic_efc_ind_con_var <- list()
-pR2_efc_ind_con_var <- list()
-
-
-
-
-
 ############################ EFECTOS CONSTANTES #####################
-
-#vect_names <- bloqueo %>% select(starts_with("VAS")) %>% select(-ends_with("DIS"),-VAS_AB) %>% names()
-#bloqueo_df <- funct_base(bloqueo, vect_names, i)
-#n <- nrow(bloqueo_df)
-#var_ini <- bloqueo_df$VAS_AB
-#var_in <-vect_names[i]
-#print(var_in)
-#fun_efc_ctes(var_in,bloqueo_df, var_ini )
-
-fun_efc_ctes <- function(var_in, bloqueo_df, var_ini, dic_efc_ctes, pR2_efc_ctes, n) {
+fun_efc_ctes <- function(var_in, bloqueo_df, var_ini, n) {
   #-Defining data-
-  data<-list("n"=n,"y"= pull(bloqueo_df[var_in]),"vasab"=var_ini)
+  data<-list("n"=n,"y"= pull(bloqueo_df[var_in] +1 ),"vasab"=var_ini +1 )
   inits_efc_con <- function(){list(theta=1,yf1=rep(1,n)) }
   par_efc_con <-c("theta","yf1")
   
@@ -96,13 +64,13 @@ fun_efc_ctes <- function(var_in, bloqueo_df, var_ini, dic_efc_ctes, pR2_efc_ctes
   # Resultado
   mod_efc_con_jags
   
-  #traceplot(mod_efc_con_jags)
+ 
   out_mod_efc_con_simulaciones <- mod_efc_con_jags$BUGSoutput$sims.list
   out_mod_efc_con_resumen <- mod_efc_con_jags$BUGSoutput$summary 
   out_mod_efc_con_dic <- mod_efc_con_jags$BUGSoutput$DIC; 
 
   
-  #dic_efc_ctes[var_in] <- out_mod_efc_con_dic
+
   
   names <- rownames(out_mod_efc_con_resumen)
   efc_con_pred <-  as.data.frame(out_mod_efc_con_resumen) %>% 
@@ -110,7 +78,7 @@ fun_efc_ctes <- function(var_in, bloqueo_df, var_ini, dic_efc_ctes, pR2_efc_ctes
     tibble() %>% 
     select(mean, names) %>% 
     filter(grepl('yf1', names)) %>% 
-    mutate(efc_con = mean) %>% 
+    mutate(efc_con = mean -1 ) %>% 
     select(efc_con)
   
   df <- bloqueo_df %>% cbind(efc_con_pred)
@@ -122,28 +90,26 @@ fun_efc_ctes <- function(var_in, bloqueo_df, var_ini, dic_efc_ctes, pR2_efc_ctes
     coord_equal() %>% 
     labs(x=var_in, title= paste ("Efectos constantes: Y vs Y predicha ",var_in) )
  
-  
-  # PSEUDO R2
-  #pR2_efc_ctes[var_in]  <- cor(pull(bloqueo_df[var_in]) , pull(efc_con_pred))^2
-  
+
   modelo <- list()
-  ##modelo["summary"] <-  as.matrix( out_mod_efc_con_resumen)
   modelo["n"] <- n
   modelo["modelo"] <- "Efectos constantes"
   modelo["var_in"]<-  var_in
   modelo["DIC"]<- out_mod_efc_con_dic
   modelo["R2"]<- cor(pull(bloqueo_df[var_in]) , pull(efc_con_pred))^2
-  print(modelo)
-  print(ggp)
+
+  print(paste ("Efectos constantes:  ",var_in))
+  print(out_mod_efc_con_resumen)
+  print(ggp)  
   
   return(modelo)
   
 }
 
 ############################ EFECTOS INDEPENDIENTES  #####################
-fun_efc_ind <- function(var_in, bloqueo_df) {
+fun_efc_ind <- function(var_in, bloqueo_df, var_ini, n) {
   #-Defining data-
-  data<-list("n"=n,"y"= pull(bloqueo_df[var_in]) ,"vasab"= var_ini)
+  data<-list("n"=n,"y"= pull(bloqueo_df[var_in] + 1)  ,"vasab"= var_ini +1 )
   inits_efc_ind <- function(){list(theta=rep(1,n),yf1=rep(1,n))}
   pars_ind<-c("theta","yf1")
   
@@ -165,15 +131,13 @@ fun_efc_ind <- function(var_in, bloqueo_df) {
   out_mod_efc_ind_resumen <- mod_efc_ind_jags$BUGSoutput$summary 
   out_mod_efc_ind_dic <- mod_efc_ind_jags$BUGSoutput$DIC ; 
 
-  dic_efc_ind[var_in]  <- out_mod_efc_ind_dic
-  
   names <- rownames(out_mod_efc_ind_resumen)
   efc_ind_pred <-  as.data.frame(out_mod_efc_ind_resumen) %>% 
     cbind(names)  %>% 
     tibble() %>% 
     select(mean, names) %>% 
     filter(grepl('yf1', names)) %>% 
-    mutate(efc_ind = mean) %>% 
+    mutate(efc_ind = mean -1) %>% 
     select(efc_ind)
   
   df <- bloqueo_df %>% cbind(efc_ind_pred)
@@ -184,17 +148,29 @@ fun_efc_ind <- function(var_in, bloqueo_df) {
     geom_point() +
     coord_equal() %>% 
     labs(x=var_in, title= paste ("Efectos independientes: Y vs Y predicha ",var_in) )
-    
+
+  # PSEUDO R2
+  
+  modelo <- list()
+  modelo["n"] <- n
+  modelo["modelo"] <- "Efectos independientes"
+  modelo["var_in"]<-  var_in
+  modelo["DIC"]<- out_mod_efc_ind_dic
+  modelo["R2"]<- cor( pull(bloqueo_df[var_in]), pull(efc_ind_pred))^2
+  
+  print(paste ("Efectos independientes: ",var_in))
+  print(out_mod_efc_ind_resumen)
   print(ggp)
   
-  # PSEUDO R2
-  pR2_efc_ind[var_in] <- cor( pull(bloqueo_df[var_in]), pull(efc_ind_pred))^2
+  return(modelo)
+  
+  
 }
 
 ############################ EFECTOS INTERCAMBIABLES   #####################
-fun_efc_int <- function(var_in, bloqueo_df) {
+fun_efc_int <- function(var_in, bloqueo_df, var_ini, n) {
   #-Defining data-
-  data<-list("n"=n,"y"= pull(bloqueo_df[var_in]),"vasab"= var_ini )
+  data<-list("n"=n,"y"= pull(bloqueo_df[var_in]+1),"vasab"= var_ini+1 )
   inits_efc_inter <-function(){list(theta=rep(1,n),a=1,b=1,yf1=rep(1,n))}
   pars_inter <-c("theta","eta","yf1")
   
@@ -216,7 +192,7 @@ fun_efc_int <- function(var_in, bloqueo_df) {
   out_mod_efc_inter_resumen <- mod_efc_inter_jags$BUGSoutput$summary 
   out_mod_efc_inter_dic <- mod_efc_inter_jags$BUGSoutput$DIC; 
   
-  dic_efc_int[var_in] <- out_mod_efc_inter_dic
+
   
   names <- rownames(out_mod_efc_inter_resumen)
   efc_inter_pred <-  as.data.frame(out_mod_efc_inter_resumen) %>% 
@@ -224,7 +200,7 @@ fun_efc_int <- function(var_in, bloqueo_df) {
     tibble() %>% 
     select(mean, names) %>% 
     filter(grepl('yf1', names)) %>% 
-    mutate(efc_inter = mean) %>% 
+    mutate(efc_inter = mean-1) %>% 
     select(efc_inter)
   
   df <- bloqueo_df %>% cbind(efc_inter_pred)
@@ -235,25 +211,37 @@ fun_efc_int <- function(var_in, bloqueo_df) {
     geom_point() +
     coord_equal() %>% 
     labs(x=var_in, title= paste ("Efectos intercambiables: Y vs Y predicha ",var_in) )
-  
-  print(ggp)
+
   
   # PSEUDO R2
-  pR2_efc_int[var_in] <- cor(pull(bloqueo_df[var_in]),  pull(efc_inter_pred))^2
+
+  
+  modelo <- list()
+  ##modelo["summary"] <-  as.matrix( out_mod_efc_con_resumen)
+  modelo["n"] <- n
+  modelo["modelo"] <- "Efectos intercambiables"
+  modelo["var_in"]<-  var_in
+  modelo["DIC"]<- out_mod_efc_inter_dic
+  modelo["R2"]<-  cor(pull(bloqueo_df[var_in]),  pull(efc_inter_pred))^2
+  
+  print(paste ("Efectos intercambiables ",var_in))
+  print(out_mod_efc_inter_resumen)
+  print(ggp)
+  
+  return(modelo)
 
 }
-  
-  
+ 
 ############################ EFECTOS INDEPENDIENTES CON VARIABLES   #####################
-fun_efc_ind_con_var <- function(var_in, bloqueo_df) {
+fun_efc_ind_con_var <- function(var_in, bloqueo_df, var_ini, n) {
   data<-list(
     "n"=n,
-    "y"=pull(bloqueo_df[var_in]),
+    "y"=pull(bloqueo_df[var_in]+1),
     "sexo"=bloqueo_df$Sexo,
     "edad"=bloqueo_df$GrupoEdad,
     "cancer"=bloqueo_df$TipoCancer2,
     "imc"=bloqueo_df$GrupoIMC, 
-    "vasab" = var_ini)
+    "vasab" = var_ini+1)
   
   inits_efc_ind_var <- function(){
     list(
@@ -291,7 +279,6 @@ fun_efc_ind_con_var <- function(var_in, bloqueo_df) {
   out_mod_efc_ind_var_resumen <- mod_efc_ind_var_jags$BUGSoutput$summary 
   out_mod_efc_ind_var_dic <- mod_efc_ind_var_jags$BUGSoutput$DIC
 
-  dic_efc_ind_con_var[var_in] <- out_mod_efc_ind_var_dic
   
   names <- rownames(out_mod_efc_ind_var_resumen)
   efc_ind_var_pred <-  as.data.frame(out_mod_efc_ind_var_resumen) %>% 
@@ -299,7 +286,7 @@ fun_efc_ind_con_var <- function(var_in, bloqueo_df) {
     tibble() %>% 
     select(mean, names) %>% 
     filter(grepl('yf1', names)) %>% 
-    mutate(efc_ind_var = mean) %>% 
+    mutate(efc_ind_var = mean -1 ) %>% 
     select(efc_ind_var)
   
   df <- bloqueo_df %>% cbind(efc_ind_var_pred)
@@ -310,53 +297,70 @@ fun_efc_ind_con_var <- function(var_in, bloqueo_df) {
     geom_point() +
     coord_equal() %>% 
     labs(x=var_in, title= paste ("Efectos independientes con variables: Y vs Y predicha ",var_in) )
+
+  
+  
+  modelo <- list()
+  modelo["n"] <- n
+  modelo["modelo"] <- "Efectos independientes con variables"
+  modelo["var_in"]<-  var_in
+  modelo["DIC"]<- out_mod_efc_ind_var_dic
+  modelo["R2"]<- cor(pull(bloqueo_df[var_in]),  pull(efc_ind_var_pred))^2
+
+  
+  print(paste ("Efectos independientes con variables:  ",var_in))
+  print(out_mod_efc_ind_var_resumen)
   print(ggp)
   
-  # PSEUDO R2
-  pR2_efc_ind_con_var <<- cor(pull(bloqueo_df[var_in]),  pull(efc_ind_var_pred))^2
+  return(modelo)
 }
 
-### ------------------------------ Función que corre todos los modelos para cada período
 
 
-#i=2
-#bloqueo_df <- funct_base(bloqueo, vect_names, i)
-#n <- nrow(bloqueo_df); n
-#var_ini <- bloqueo_df$VAS_AB
-#var_in <-vect_names[i] 
 
-
-       
-i=1   
+############################ ITERACION SOBRE TODOS LOS MODELOS   #####################
+     
+  
 vect_names <- bloqueo %>% select(starts_with("VAS")) %>% select(-ends_with("DIS"),-VAS_AB) %>% names()
 
-
-modelos finales
-
 modelos<- function( vect_names,bloqueo) {
+  
   modelos_fin <- list()
   
-  
-  bloqueo_df <- funct_base(bloqueo, vect_names, i)
-  print(bloqueo_df)
-  n <- nrow(bloqueo_df)
-  var_ini <- bloqueo_df$VAS_AB
-  var_in <-vect_names[i]
-  print(var_in)
-  modelos_fin[var_in] <- fun_efc_ctes(var_in, bloqueo_df, var_ini, dic_efc_ctes, pR2_efc_ctes, n)
+  for (i in 1:length(vect_names)) {
+    
+    #Parametros de cada modelo  
+    bloqueo_df <- funct_base(bloqueo, vect_names, i)
+            n <- nrow(bloqueo_df)
+    var_ini <- bloqueo_df$VAS_AB
+    var_in <-vect_names[i]
 
-  
+    # Modelos
+    modelo_con <- fun_efc_ctes(var_in, bloqueo_df, var_ini, n)
+    nombre <- paste(modelo_con["modelo"], var_in )
+    modelos_fin[[nombre]] <- modelo_con
+    
+    modelo_ind <- fun_efc_ind(var_in, bloqueo_df, var_ini, n)
+    nombre <- paste(modelo_ind["modelo"], var_in )
+    modelos_fin[[nombre]] <- modelo_ind
+    
+    modelo_inter <- fun_efc_int(var_in, bloqueo_df, var_ini, n)
+    nombre <- paste(modelo_inter["modelo"], var_in )
+    modelos_fin[[nombre]] <- modelo_inter
+    
+    modelo_ind_var <- fun_efc_ind_con_var(var_in, bloqueo_df, var_ini, n)
+    nombre <- paste(modelo_ind_var["modelo"], var_in )
+    modelos_fin[[nombre]] <- modelo_ind_var
   
   }
-  #fun_efc_ind(var_in, bloqueo_df)
-  #fun_efc_int(var_in, bloqueo_df)
-  #fun_efc_ind_con_var(var_in,bloqueo_df)
+    modelos_fin
 }
 
 
 
 
-modelos ( i, vect_names,bloqueo)
+modelos_todos<- modelos ( vect_names,bloqueo)
+modelos_todos
 
 
 
